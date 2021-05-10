efb881f2b53c6ce82478278eb39cdd7d059ceeac
Richard <rdesilvey@gmail.com>
2021-05-10 17:18:02 -0600

    Update Relationships

--- db/migrate/20210510225921_create_schools.rb
+++ db/migrate/20210510225921_create_schools.rb
@@ -2,8 +2,8 @@ class CreateSchools < ActiveRecord::Migration[5.2]
   def change
     create_table :schools do |t|
       t.string :name
-      t.boolean :is_remote
-      t.integer :school_code
+      t.boolean :is_remote, default: false
+      t.integer :school_code, unique: true
 
       t.timestamps
     end
--- db/migrate/20210510230022_create_students.rb
+++ db/migrate/20210510230022_create_students.rb
@@ -2,8 +2,9 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.string :name
-      t.boolean :alumni
-      t.integer :badge_code
+      t.boolean :alumni, default: false
+      t.integer :badge_code, unique: true
+      t.belongs_to :school, foreign_key: true
 
       t.timestamps
     end
