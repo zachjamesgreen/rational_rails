4984aee47e7c3408d323fa7676f6d904bc45ec9a
Richard <rdesilvey@gmail.com>
2021-05-10 17:50:04 -0600

    Test model

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe School, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'instance methods' do
+    describe '#remote?' do
+      it 'returns Yes if the school is remote' do
+        school = School.create!(name: 'Turing', is_remote: true, school_code: 1)
+        expect(school.remote?).to eq 'Yes'
+      end
+
+      it 'returns No if the school is not remote' do
+        school = School.create!(name: 'Turing', is_remote: true, school_code: 1)
+        expect(school.remote?).to eq 'No'
+      end
+    end
+  end
 end
