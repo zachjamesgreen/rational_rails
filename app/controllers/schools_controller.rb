d268ba1b57bd2fbe8d8f505736589fc8e70355e6
Richard <rdesilvey@gmail.com>
2021-05-11 11:30:26 -0600

    Update Controllers

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,13 +1,51 @@
 class SchoolsController < ApplicationController
   def index
-    @schools = School.order(:created_at)
+    @schools = School.order(created_at: :desc)
+  end
+
+  def new
+  end
+
+  def create
+    attrs = params[:school]
+    School.create(name: attrs[:name],
+                  school_code: attrs[:school_code],
+                  is_remote: attrs[:is_remote])
+
+    redirect_to '/schools'
   end
 
   def show
     @school = School.find(params[:id])
   end
 
+  def update
+    @school = School.find(params[:id])
+    @school.update_attributes(school_params)
+    @school.save
+    redirect_to '/schools'
+  end
+
+  def edit
+    @school = School.find(params[:id])
+  end
+
   def students
     @school = School.find(params[:id])
+  end
+
+  def degrees
+    @school = School.find(params[:id])
+    @current_degrees = @school.students.each_with_object([]) do |student, degrees|
+      degrees << student.degree
+    end.sort
+  end
+
+  private
+
+  def school_params
+    if params[:school]
+      params.require(:school).permit(:school, :name, :school_code, :is_remote)
+    end
   end
 end
