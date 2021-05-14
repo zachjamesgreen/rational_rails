50717f2a8be7f2a4f5d8c979ad6c60563c4cd7a3
Richard <rdesilvey@gmail.com>
2021-05-14 13:39:13 -0600

    Move describe block out

--- spec/features/school/students_spec.rb
+++ spec/features/school/students_spec.rb
@@ -9,22 +9,24 @@ RSpec.describe 'The list of students for school,' do
       it 'submits request and shows new list of students based on given query value'
     end
   end
+
+  describe 'clickable button,' do
+    describe 'view student,' do
+      it 'is the name of the student and is clickable'
+      it 'redirects to /students/:id'
+    end
+    describe 'Edit student,' do
+      it 'can be seen next to each student'
+      it 'redirects to /students/:id/edit'
+    end
+    describe 'Delete student,' do
+      it 'can be seen next to each student'
+      it 'deletes student and redirects to /schools/:id/students without the deleted student listed anymore'
+    end
+  end
+  
   describe 'student list,' do
     it 'has the expected headers'
     it 'shows all students for the school'
-    describe 'clickable button,' do
-      describe 'view student,' do
-        it 'is the name of the student and is clickable'
-        it 'redirects to /students/:id'
-      end
-      describe 'Edit student,' do
-        it 'can be seen next to each student'
-        it 'redirects to /students/:id/edit'
-      end
-      describe 'Delete student,' do
-        it 'can be seen next to each student'
-        it 'deletes student and redirects to /schools/:id/students without the deleted student listed anymore'
-      end
-    end
   end
 end
