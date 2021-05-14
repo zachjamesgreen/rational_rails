4e5ac33156cb27b855196524fb2a26864996f512
Richard <rdesilvey@gmail.com>
2021-05-14 12:56:01 -0600

    Add after hooks

--- spec/features/school/degrees_spec.rb
+++ spec/features/school/degrees_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'show degrees' do
-  before :each do
+RSpec.describe 'The show degrees page' do
+  before :all do
     @school = School.create!(name: 'Turing', is_remote: true, school_code: 1)
     @school.students.create!(name: 'Rich', age: 33, is_alumni: true, degree: 'Computer Science')
     @school.students.create!(name: 'Dustin', age: 35, is_alumni: false, degree: 'Geology')
   end
 
-  it 'have an unordered lists of degrees on the page' do
+  after :all do
+    School.destroy_all
+  end
+
+  it 'has an unordered list visible' do
     visit "/schools/#{@school.id}/degrees"
     degree_list = page.find('ul.degree-list')
 
     expect(degree_list).to have_selector('li', count: 2)
   end
 
-  it 'has a list of all pursued degrees' do
+  it 'has a list of all pursued degrees for the selected school' do
     visit "/schools/#{@school.id}/degrees"
 
     degree_list = page.find('ul.degree-list')
--- spec/features/school/edit_spec.rb
+++ spec/features/school/edit_spec.rb
@@ -1,48 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe 'the school edit' do
-  describe 'page' do
-    before :all do
-      @turing_school = School.create!(name: 'Turing', school_code: 1, is_remote: true)
-      @msu_school = School.create!(name: 'MSU Denver', school_code: 2, is_remote: false)
+RSpec.describe 'The school edit page' do
+  before :all do
+    @turing_school = School.create!(name: 'Turing', school_code: 1, is_remote: true)
+    @msu_school = School.create!(name: 'MSU Denver', school_code: 2, is_remote: false)
+  end
+
+  after :all do
+    School.destroy_all
+  end
+
+  it 'shows a title with the school name' do
+    School.all.each do |school|
+      visit "/schools/#{school.id}/edit"
+
+      page_title = page.find('h2')
+      expect(page_title).to have_content("Edit School #{school.name}")
+    end
+  end
+
+  describe 'edit form,' do
+    it 'is present' do
+      visit "/schools/#{@turing_school.id}/edit"
+      expect(page.find('form')).to be_present
     end
 
-    it 'shows a title with the school name' do
+    it 'has populated fields of the school' do
       School.all.each do |school|
         visit "/schools/#{school.id}/edit"
 
-        page_title = page.find('h2')
-        expect(page_title).to have_content("Edit School #{@school.name}")
+        expect(page.find(:id, 'school_name').value).to eq(school.name)
+        expect(page.find(:id, 'school_code').value).to eq(school.school_code.to_s)
+        expect(page.find(:id, 'school_is_remote').value).to eq(school.is_remote.to_s)
       end
     end
 
+    it 'navigates back to the edited school when form is submitted with new data visible' do
+      visit "/schools/#{@turing_school.id}/edit"
 
-    describe 'form' do
-      it 'is present' do
-        visit "/schools/#{@turing_school.id}/edit"
-        expect(page.find('form')).to be_present
+      within 'form' do
+        fill_in 'school[name]', with: Faker::Educator.university
+        find(:id, 'submit_button').click
       end
 
-      it 'has populated fields of the school' do
-        School.all.each do |school|
-          visit "/schools/#{school.id}/edit"
-
-          expect(page.find(:id, 'school_name').value).to eq(school.name)
-          expect(page.find(:id, 'school_code').value).to eq(school.school_code.to_s)
-          expect(page.find(:id, 'school_is_remote').value).to eq(school.remote?)
-        end
-      end
-
-      it 'navigates back to the edited school when form is submitted with new data visible' do
-        visit "/schools/#{@turing_school.id}/edit"
-
-        within 'form' do
-          fill_in 'school[name]', with: Faker::Educator.university
-          find(:id, 'submit_button').click
-        end
-
-        current_path.should eq "/schools/#{@turing_school.id}"
-      end
+      current_path.should eq "/schools/#{@turing_school.id}"
     end
   end
 end
