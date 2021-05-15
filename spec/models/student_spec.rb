be090c05b24ddd92c49af8e2f1ca56fb3705c534
Richard <rdesilvey@gmail.com>
2021-05-14 18:00:06 -0600

    Fix test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -7,14 +7,20 @@ RSpec.describe Student, type: :model do
   end
 
   describe 'instance methods' do
+    before :all do
+      @test_school = School.create!(name:'Turing', is_remote: true, school_code: 1)
+    end
+    after :all do
+      @test_school.destroy
+    end
     describe '#has_graduated?' do
       it 'returns Yes if this student is an alumni' do
-        student = Student.create!(name:'Dustin', age:1, is_alumni:true)
+        student = @test_school.students.create!(name:'Dustin', age:1, is_alumni:true)
         expect(student.has_graduated?).to eq 'Yes'
       end
 
       it 'returns No if this student is not an alumni' do
-        student = Student.create!(name:'Dustin', age:1, is_alumni:false)
+        student = @test_school.students.create!(name:'Dustin', age:1, is_alumni:false)
         expect(student.has_graduated?).to eq 'No'
       end
     end
