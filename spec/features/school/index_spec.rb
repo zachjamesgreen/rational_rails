0973b7bc22651db5b7aa985714a6e29b81ba6698
Richard <rdesilvey@gmail.com>
2021-05-20 10:35:36 -0600

    Clean up index spec
    For readability and maintainability

--- spec/features/school/index_spec.rb
+++ spec/features/school/index_spec.rb
@@ -18,36 +18,32 @@ RSpec.describe 'The school index page,' do
     title = page.find('h2')
 
     expect(title).to have_content('List of All Schools')
+    expect(title).to appear_before(page.find('#schools_table'))
   end
 
   describe 'clickable button' do
     describe 'Add school,' do
-      before :each do
-        top_table = page.find(:id, 'header_table')
-        @create_button = top_table.find('a')
-      end
       it 'should be seen at the top of the page' do
-        expect(@create_button.text).to eq 'Add New School'
+        within '#header_table' do
+          expect(page.find('a')).to have_content('Add New School')
+        end
       end
 
       it 'redirects the user to /schools/new' do
-        @create_button.click
+        page.find('#add_school_btn').click
         current_path.should eq '/schools/new'
       end
     end
 
     describe 'Edit school,' do
-      before :each do
-        schools_table = page.find(:id, 'schools_table')
-        @edit_button = schools_table.find(:id, "edit_school_#{@school_1.id}_btn")
-      end
-
       it 'should be seen next to each school' do
-        expect(@edit_button).to have_content('Edit')
+        within "#button_row_#{@school_1.id}" do
+          expect(page.find("#edit_school_#{@school_1.id}_btn")).to have_content('Edit')
+        end
       end
 
       it 'redirects the user to /schools/:id/edit' do
-        @edit_button.click
+        page.find("#edit_school_#{@school_1.id}_btn").click
         current_path.should eq "/schools/#{@school_1.id}/edit"
       end
     end
@@ -57,74 +53,51 @@ RSpec.describe 'The school index page,' do
         @test_school = School.create!(name: 'Greg University', is_remote: false, school_code: 10)
       end
 
-      before :each do
-        @schools_table = page.find(:id, 'schools_table')
-      end
-
       after :all do
         @test_school.destroy!
       end
 
       it 'should be seen next to each school' do
-        delete_button = @schools_table.find(:id, "delete_school_#{@test_school.id}_btn")
+        delete_button = page.find("#delete_school_#{@test_school.id}_btn")
         expect(delete_button).to have_content('Delete')
       end
 
       it 'redirects the user to /schools and will no longer see the school listed' do
-        table_rows = @schools_table.find_all('tr')
-        delete_button = @schools_table.find(:id, "delete_school_#{@test_school.id}_btn")
 
-        expected_data_seen_before_deletion = [
-          [@test_school.name, @test_school.created_at.to_s],
-          [@school_1.name, @school_1.created_at.to_s],
-          [@school_2.name, @school_2.created_at.to_s]]
+        delete_button = page.find("#delete_school_#{@test_school.id}_btn")
 
-        expected_data_seen_after_deletion = [
-          [@school_1.name, @school_1.created_at.to_s],
-          [@school_2.name, @school_2.created_at.to_s]]
-
-        actual_data_seen_before_deletion = table_rows.filter_map do |row|
-          table_data = row.find_all('td')
-          table_data.map(&:text) if table_data.length > 1
+        within '#schools_table' do
+          expect(page).to have_content(@test_school.name)
         end
-
-        expect(actual_data_seen_before_deletion).to eq expected_data_seen_before_deletion
 
         delete_button.click
 
-        table_rows = @schools_table.find_all('tr')
-
-        actual_data_seen_after_deletion = table_rows.filter_map do |row|
-          table_data = row.find_all('td')
-          table_data.map(&:text) if table_data.length > 1
-        end
-
         current_path.should eq '/schools'
-        expect(actual_data_seen_after_deletion).to eq expected_data_seen_after_deletion
+
+        within '#schools_table' do
+          expect(page).not_to have_content(@test_school.name)
+        end
       end
     end
   end
 
   describe 'schools list,' do
-    it 'has all the schools listed' do
+    it 'has all the schools listed in decending order by creation date' do
       schools_table = page.find(:id, 'schools_table')
-      table_rows = schools_table.find_all('tr')
 
-      expected_data_seen = [
-        [@school_1.name, @school_1.created_at.to_s],
-        [@school_2.name, @school_2.created_at.to_s]]
+      school_row_1 = find("#school_data_row_#{@school_1.id}")
+      school_row_2 = find("#school_data_row_#{@school_2.id}")
 
-      actual_data_seen = table_rows.filter_map do |row|
-        table_data = row.find_all('td')
-        table_data.map(&:text) if table_data.length > 1
+      within '#schools_table' do
+        expect(page).to have_content('School Name')
+        expect(page).to have_content('Creation Date')
+        expect(school_row_1).to have_content(@school_1.name)
+        expect(school_row_1).to have_content(@school_1.created_at.to_s)
+        expect(school_row_2).to have_content(@school_2.name)
+        expect(school_row_2).to have_content(@school_2.created_at.to_s)
       end
 
-      expected_headers_seen = ['School Name', 'Creation Date']
-
-      actual_headers_seen = table_rows.first.find_all('th').map(&:text)
-
-      expect(actual_data_seen).to eq expected_data_seen
-      expect(actual_headers_seen).to eq expected_headers_seen
+      expect(school_row_1).to appear_before(school_row_2)
     end
   end
 end
