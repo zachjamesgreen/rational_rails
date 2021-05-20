7ee169513a4157ef2d8ce76b3a794a53b9e12bb2
Richard <rdesilvey@gmail.com>
2021-05-20 11:29:13 -0600

    Clean up student show spec

--- spec/features/student/show_spec.rb
+++ spec/features/student/show_spec.rb
@@ -17,40 +17,45 @@ RSpec.describe 'The show student page,' do
 
   describe 'attributes of the student,' do
     it 'has expected headers' do
-      headers = page.find(:id, 'header_row').find_all('th')
-
-      expected_content = ['Student ID', 'Student Name', 'Age', 'Attending School', 'Degree', 'Graduate']
-      seen_content = headers.map(&:text)
-
-      expect(seen_content).to eq expected_content
+      within '#header_row' do
+        expect(page).to have_content('Student ID')
+        expect(page).to have_content('Student Name')
+        expect(page).to have_content('Age')
+        expect(page).to have_content('Attending School')
+        expect(page).to have_content('Degree')
+        expect(page).to have_content('Graduate')
+      end
     end
 
     it 'shows the attributes' do
-      expect(page.find(:id, 'student_id')).to have_content(@test_student.id)
-      expect(page.find(:id, 'student_name')).to have_content(@test_student.name)
-      expect(page.find(:id, 'student_age')).to have_content(@test_student.age)
-      expect(page.find(:id, 'student_school')).to have_content(@test_student.school.name)
-      expect(page.find(:id, 'student_degree')).to have_content(@test_student.degree)
-      expect(page.find(:id, 'student_graduated')).to have_content(@test_student.has_graduated?)
+      expect(page.find('#student_id')).to have_content(@test_student.id)
+      expect(page.find('#student_name')).to have_content(@test_student.name)
+      expect(page.find('#student_age')).to have_content(@test_student.age)
+      expect(page.find('#student_school')).to have_content(@test_student.school.name)
+      expect(page.find('#student_degree')).to have_content(@test_student.degree)
+      expect(page.find('#student_graduated')).to have_content(@test_student.has_graduated?)
     end
   end
   describe 'clickable buttons,' do
+    describe 'Attending School,' do
+      it 'navigates to the show school page' do
+        find('#show_school').click
+        current_path.should eq "/schools/#{@test_student.school_id}"
+      end
+    end
+
     describe 'Update,' do
       it 'navigates to /students/:id/edit' do
-        find(:id, 'edit_student_btn').click
+        find('#edit_student_btn').click
         current_path.should eq "/students/#{@test_student.id}/edit"
       end
     end
 
     describe 'Delete,' do
       it 'deletes student and will not be visible anymore at /students' do
-        find(:id, 'delete_student_btn').click
-
-        begin
-          fail if page.find(:id, "student_data_row_#{@test_student.id}")
-        rescue
-          current_path.should eq '/students'
-        end
+        deleted_name = @test_student.name
+        find('#delete_student_btn').click
+        expect(page).not_to have_content(deleted_name)
       end
     end
   end
